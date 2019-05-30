c331ce2f3413f5ec8628eb53911c933d2bcdee35
Dendi Maniac <daq30899@gmail.com>
2019-05-30 22:03:11 +0300

    Initial commit, with basic movement for player

---
+++ Assets/Scripts/GetTilePos.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GetTilePos : MonoBehaviour
+{
+    [HideInInspector]
+    public Tilemap tileMap = null;
+
+    void Start()
+    {
+        tileMap = transform.GetComponentInParent<Tilemap>();
+        MovementManagement.GetTilePos(tileMap);
+    }
+}
