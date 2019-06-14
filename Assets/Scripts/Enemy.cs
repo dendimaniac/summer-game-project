6814b2d11e1356d87a357d3639a5f2735bb56818
Dendi Maniac <daq30899@gmail.com>
2019-06-14 20:33:04 +0300

    Improve on the bots movement code

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
 public class Enemy : MovingObject
 {
     public Transform player;
     public PlayerPos moveRoute;
+    private bool isMoving = false;
+    private bool isDoneMoving = false;
     private void Update()
     {
-        if (player.position == new Vector3(-4, 0, 0))
+        if (player.position == new Vector3(-4, 0, 0) && !isDoneMoving)
         {
-            Vector3 nextPos;
-            for (int i = 0; i < moveRoute.positionList.Count; i++)
-            {
-                nextPos = new Vector3(moveRoute.positionList[i][0], moveRoute.positionList[i][1], moveRoute.positionList[i][2]);
-                StartCoroutine(SmoothMovement(nextPos));
-            }
+            if (!isMoving)
+                StartCoroutine(toFollow());
         }
+    }
+    private IEnumerator toFollow()
+    {
+        isMoving = true;
+        Vector3 nextPos;
+        for (int i = 0; i < moveRoute.positionList.Count; i++)
+        {
+            nextPos = new Vector3(moveRoute.positionList[i][0], moveRoute.positionList[i][1], moveRoute.positionList[i][2]);
+            if (i == moveRoute.positionList.Count - 1)  isDoneMoving = true;
+            yield return StartCoroutine(SmoothMovement(nextPos));
+        }
+        isMoving = false;
     }
     protected override void OnCantMove<T>(T component)
     {
